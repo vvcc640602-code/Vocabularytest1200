5544779800cb1661afc6be8ac2348375fac167d4
vvcc640602-code <vvcc640602@gmail.com>
2026-09-01 21:21:05 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.QUIZ_CONFIG = {
-  sheetEndpoint: "https://script.google.com/macros/s/AKfycbyB0_falwNIFtl7kofHMVVIml8TLpT20bt44C4q9nJKk7lU0utPtgEpLKOE4AM3MZUQgw/exec"
+  sheetEndpoint: "https://script.google.com/macros/s/AKfycbzk7H8soTX1HDqTPjf55sGi9KLrw5-EXrAlDH1DHbUAIxyx_DYfJCERUfMSlVe2rLxrjQ/exec/exec"
 };
